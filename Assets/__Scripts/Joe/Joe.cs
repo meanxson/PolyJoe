734cdd39211ad2b89eaf41a636f6450180dc8dc8
Myrzabek Alizhanov <42093747+meanxson@users.noreply.github.com>
2021-07-21 08:43:03 +0300

    Update Joe.cs

--- Assets/__Scripts/Joe/Joe.cs
+++ Assets/__Scripts/Joe/Joe.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Joe : MonoBehaviour
 {
-    public static Joe This;
-    private MultiTargetCam _cam;
-    private void Awake()
+    public static Joe This; //FUCKING SIGNLETON DESTROY THIS SHIT
+    private MultiTargetCam _cam; //+rep
+    private void Awake() 
     {
         _cam = Camera.main.GetComponent<MultiTargetCam>();
         _cam.targets.Add(transform);
