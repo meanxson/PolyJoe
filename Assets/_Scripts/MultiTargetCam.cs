e2137ee296ace9816b7b53672b299a304fae93bf
Myrzabek Alizhanov <42093747+meanxson@users.noreply.github.com>
2021-07-21 08:35:34 +0300

    Refactored and Optimized
    - Code guideline
    - Moving with observerable collection
    -  Coroutine

--- Assets/_Scripts/MultiTargetCam.cs
+++ Assets/_Scripts/MultiTargetCam.cs
@@ -1,44 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using UnityEngine;
 
 [RequireComponent(typeof(Camera))]
 public class MultiTargetCam : MonoBehaviour
 {
-    public List<Transform> targets;
-    private Vector3 velociy;
-    public float smoothTime;
-    public Vector3 offset;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private float smoothTime;
+    [SerializeField] private List<Transform> _targets;
+    
+    private Vector3 _velocity;
+    private ObservableCollection<Transform> _observableCollection { get; set; }
 
+    private void Awake()
+    {
+        _observableCollection = new ObservableCollection<Transform>(_targets);
+        
+        //Subscribe to event when _targets is changed
+        _observableCollection.CollectionChanged += (sender, args) =>
+        {
+            StartCoroutine(Move());
+        };
+    }
 
-    void Move()
+    private IEnumerator Move()
     {
         Vector3 centerPoint = GetCenterPoint();
-
         Vector3 newPosition = centerPoint + offset;
-
-        transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velociy, smoothTime);
-    }
-   
-    private void LateUpdate()
-    {
-        if (targets.Count == 0)
-            return;
-
-        Move();
+        Vector3 dis = transform.position - newPosition;
+        
+        while (dis.sqrMagnitude > 0.1f*0.1f)
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref _velociy, smoothTime);
+            yield return new WaitForEndOfFrame();
+        }
     }
 
-    Vector3 GetCenterPoint()
+    private Vector3 GetCenterPoint()
     {
-        if (targets.Count == 1)
-        {
-            return targets[0].position;
-        }
-        var bounds = new Bounds(targets[0].position, Vector3.zero);
-        for(int i = 0; i < targets.Count; i++)
-        {
-            bounds.Encapsulate(targets[i].position);
-        }
+        if (_observableCollection.Count == 1)
+            return _observableCollection[0].position;
+        
+        var bounds = new Bounds(_observableCollection[0].position, Vector3.zero);
+        foreach (var t in _observableCollection) 
+            bounds.Encapsulate(t.position);
+        
         return bounds.center;
     }
 }
